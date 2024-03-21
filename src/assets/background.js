627796cce51cdc178341215202f9ccf13fc192f2
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-03-21 16:25:38 +0100

    backgrounds

---
+++ src/assets/background.js
@@ -0,0 +1,16 @@
+const backgrounds = {
+    1: '/background/abstract-5791221_1280.jpg',
+    2: '/background/astronomy-1867616_1280.jpg',
+    3: '/background/background-7275953_1280.jpg',
+    4: '/background/bird-of-paradise-flower-302889_1280.jpg',
+    5: '/background/black-1072366_1280.jpg',
+    6: '/background/bricks-459299_1280.jpg',
+    7: '/background/dewdrops-3671613_1280.jpg',
+    8: '/background/fern-1250903_1280.jpg',
+    9: '/background/forest-3119826_1280.jpg',
+    10: '/background/grass-84622_1280.jpg',
+    11: '/background/leaves-1419985_1280.jpg',
+    12: '/background/wall-1217083_1280.jpg',
+    13: '/background/water-1330252_1280.jpg'
+  };
+  export default backgrounds;
