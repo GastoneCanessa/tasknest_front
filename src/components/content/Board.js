e756f87b663d17b28884534c70fbc1173e9e40c9
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-03-21 19:08:08 +0100

    we

--- src/components/content/Board.js
+++ src/components/content/Board.js
@@ -21,38 +21,74 @@ export default function Board(props) {
     }, [props.board.id]); // Dipendenze useEffect
 
     const onDragEnd = (result) => {
-        const { destination, source } = result;
+        const { destination, source, type, draggableId } = result;
 
-        if (!destination || (destination.droppableId === source.droppableId && destination.index === source.index)) {
-            return;
-        }
+        // Se non c'è una destinazione, non fare nulla
+        if (!destination) return;
 
-        const newTaskLists = Array.from(board.my_tasklists);
-        const [reorderedItem] = newTaskLists.splice(source.index, 1);
-        newTaskLists.splice(destination.index, 0, reorderedItem);
+        // Gestione del riordinamento delle liste
+        if (type === "column") {
+            const newTaskLists = Array.from(board.my_tasklists);
+            const [reorderedItem] = newTaskLists.splice(source.index, 1);
+            newTaskLists.splice(destination.index, 0, reorderedItem);
 
-        setBoard({ ...board, my_tasklists: newTaskLists });
+            // Aggiorna lo stato per riflettere il nuovo ordine
+            setBoard({ ...board, my_tasklists: newTaskLists });
 
-        axios.put('/list/position', {
-            idList: reorderedItem.id,
-            newPosition: destination.index + 1
-        })
-            .then(() => {
-                console.log('Posizione aggiornata con successo.');
-                loadBoard();
+            // Chiamata Axios per aggiornare la posizione della lista nel backend
+            axios.put(`/list/position`, {
+                idList: reorderedItem.id,
+                newPosition: destination.index + 1 // Assumi che l'indice inizia da 1 nel tuo backend
             })
-            .catch((error) => {
-                console.error('Errore nell\'aggiornamento della posizione:', error);
-            });
-    };
+                .then(() => {
+                    console.log('Posizione della lista aggiornata con successo.');
+                    loadBoard()
+                })
+                .catch((error) => {
+                    console.error('Errore nell\'aggiornamento della posizione della lista:', error);
+                });
+        } else if (type === "task") {
+            const startListIndex = board.my_tasklists.findIndex(list => `droppable-tasklist-${list.title}` === source.droppableId);
+            const finishListIndex = board.my_tasklists.findIndex(list => `droppable-tasklist-${list.title}` === destination.droppableId);
 
+            const startList = board.my_tasklists[startListIndex];
+            const finishList = board.my_tasklists[finishListIndex];
+
+            // Rimuovi la task dalla sua posizione originale
+            const newStartTasks = Array.from(startList.my_tasks);
+            const [movedTask] = newStartTasks.splice(source.index, 1);
+
+            // Se la task è stata spostata all'interno della stessa lista
+            if (startList === finishList) {
+                newStartTasks.splice(destination.index, 0, movedTask); // Inserisci la task nella nuova posizione
+                const newBoard = { ...board };
+                newBoard.my_tasklists[startListIndex].my_tasks = newStartTasks;
+                setBoard(newBoard); // Aggiorna lo stato in maniera ottimistica
+            } else {
+                // Gestisci lo spostamento tra liste diverse se necessario
+            }
+
+            // Successivamente, effettua la chiamata al server per aggiornare la posizione della task
+            axios.put(`/tasks`, {
+                id: draggableId,
+                position: destination.index + 1
+            })
+                .then(() => {
+                    loadBoard();
+                })
+                .catch((error) => {
+                    console.error('Errore nell\'aggiornamento della posizione della task:', error);
+                    // Gestisci l'errore, potenzialmente ripristinando lo stato precedente o mostrando un messaggio all'utente
+                });
+        }
+    };
 
 
 
     return (
         <DragDropContext onDragEnd={onDragEnd}>
             <h1>{board.title}</h1>
-            <Droppable droppableId={board.title} direction="horizontal">
+            <Droppable droppableId={board.title} direction="horizontal" type="column">
                 {(provided) => (
                     <div
                         {...provided.droppableProps}
@@ -60,7 +96,7 @@ export default function Board(props) {
                         className="row"
                     >
                         {board.my_tasklists.map((list, index) => (
-                            <Draggable key={list.id} draggableId={String(list.id)} index={index}>
+                            <Draggable key={list.id} draggableId={String(list.title)} index={index}>
                                 {(provided) => (
                                     <div
                                         ref={provided.innerRef}
@@ -71,9 +107,17 @@ export default function Board(props) {
                                         <div className="card p-3">
                                             <p>{list.title}</p>
                                             <p>{list.position}</p>
-                                            {list.my_tasks && list.my_tasks.map((t) => (
-                                                <SingleTask t={t} padre={list} />
-                                            ))}
+                                            {/* Droppable per le task */}
+                                            <Droppable droppableId={`droppable-tasklist-${list.title}`} type="task">
+                                                {(provided) => (
+                                                    <div ref={provided.innerRef} {...provided.droppableProps}>
+                                                        {list.my_tasks && list.my_tasks.map((task, taskIndex) => (
+                                                            <DraggableTask key={task.id} task={task} index={taskIndex} />
+                                                        ))}
+                                                        {provided.placeholder}
+                                                    </div>
+                                                )}
+                                            </Droppable>
                                         </div>
                                     </div>
                                 )}
@@ -84,5 +128,26 @@ export default function Board(props) {
                 )}
             </Droppable>
         </DragDropContext>
+    );
+}
+
+function DraggableTask({ task, index }) {
+    return (
+        <Draggable draggableId={String(task.id)} index={index} >
+            {(provided) => (
+                <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className="card my-2"
+                >
+                    <div className="card-content">
+                        <p>{task.title}</p>
+                        <p>{task.position}</p>
+                        {/* Dettagli della task */}
+                    </div>
+                </div>
+            )}
+        </Draggable>
     );
 }
