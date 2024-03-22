6f8d4550e7a9fd12fe4efb7508cd2a82bf49143f
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-03-22 12:20:26 +0100

    login

--- src/components/common/BackgroundSelector.js
+++ src/components/common/BackgroundSelector.js
@@ -1,33 +1,31 @@
 import React from 'react';
-import backgrounds from '../../assets/background'; // Assicurati di inserire il percorso corretto
+import axios from 'axios';
+import backgrounds from '../../assets/background';
 
 const BackgroundSelector = () => {
-  const selectBackground = async (path) => {
-    try {
-      // Sostituisci 'your-backend-url' con l'URL effettivo del tuo backend
-      const response = await fetch('your-backend-url', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ background: path }),
-      });
-      const data = await response.json();
-      console.log(data); // Gestisci la risposta come preferisci
-    } catch (error) {
-      console.error('Error selecting background:', error);
-    }
-  };
+    const selectBackground = async (id) => {
+        try {
+            const response = await axios.put('/boards/img',
+                { 
+                    id:1,
+                    imgId: id 
+                }
+            );
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error selecting background:', error);
+        }
+    };
 
-  return (
-    <div className='row m-5'>
-      {Object.entries(backgrounds).map(([id, path]) => (
-        <div className='col' key={id} onClick={() => selectBackground(path)} style={{ cursor: 'pointer', marginBottom: '10px' }}>
-          <img className='rounded-3' src={path} alt={`Sfondo ${id}`} style={{ width: '300px', height: '180px' }} />
+    return (
+        <div className='row m-5'>
+            {Object.entries(backgrounds).map(([id, path]) => (
+                <div className='col' key={id} onClick={() => selectBackground(id)} style={{ cursor: 'pointer', marginBottom: '10px' }}>
+                    <img src={path} alt={`Sfondo ${id}`} style={{ width: '300px', height: '180px' }} />
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default BackgroundSelector;
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,19 +1,40 @@
 import { Link } from "react-router-dom";
+import { useAtom } from 'jotai';
+import { currentUser } from "../../App";
+import { useNavigate } from 'react-router';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    const [user, setUser] = useAtom(currentUser);
+    const isUserNotEmpty = Object.keys(user).length > 0;
+
+    function logOut() {
+        setUser({});
+        localStorage.setItem('user', JSON.stringify({}));
+        navigate("/");
+    }
+
+
     return (
         <div className="" style={{ background: "pink" }}>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">TaskNest</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
-                    </button>
+                    </button> */}
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
+                            {!isUserNotEmpty ? (
+                                <>
+                                    <a className="navbar-brand" href="/">TaskNest</a>
+                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                </>
+                            ) : (
+                                <>
+                                    <a className="navbar-brand" href="/user/home">TaskNest</a>
+                                    <a className="nav-link active" aria-current="page" href="/user/home">Home</a>
+                                </>
+                            )}
                             {/* <li className="nav-item">
                                 <a className="nav-link" href="#">Link</a>
                             </li>
@@ -37,7 +58,14 @@ const Navbar = () => {
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
-                        <Link className="btn  me-2" to="/user/login">Login</Link>
+
+                        {!isUserNotEmpty ? (
+                            <Link className="nav-link  fst-italic fw-semibold fs-5 login-link" to="user/login">Login</Link>
+                        ) : (
+                            <>
+                                <button className="nav-link  fst-italic fw-semibold fs-5 login-link" onClick={logOut}>Log Out</button>
+                            </>
+                        )}
                     </div>
                 </div>
             </nav>
