0ff183ff5655f408cb1c7e04f3fb6e051bd95fd1
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-03-26 17:19:38 +0100

    dajeo

--- src/components/content/NewBoardForm.js
+++ src/components/content/NewBoardForm.js
@@ -38,7 +38,7 @@ const NewBoardForm = ({ onClose, onBoardAdded }) => {
   };
 
   const selectBackground = async (id) => {
-    setBoard({ ...board, img: Number(id)});
+    setBoard({ ...board, img: Number(id) });
   };
 
   return (
@@ -48,41 +48,75 @@ const NewBoardForm = ({ onClose, onBoardAdded }) => {
       left: '50%',
       transform: 'translate(-50%, -20%)',
       width: '80vw', // Imposta la larghezza del popup
-      height: '80vh', // Imposta l'altezza del popup
-      backgroundColor: 'white',
+      minHeight: '80vh', // Imposta l'altezza del popup
+      backgroundColor: '#e9baca',
       padding: '20px',
       zIndex: 100,
-      border: '1px solid #ccc', // Aggiungi un bordo (opzionale)
+      border: '0px solid #ccc', // Aggiungi un bordo (opzionale)
       borderRadius: '5px', // Bordi arrotondati (opzionale)
     }}>
-      <form onSubmit={handleSubmit}>
-        <div className='row m-5'>
-          {Object.entries(backgrounds).map(([id, path]) => (
-            <div className='col' key={id} onClick={() => selectBackground(id)} style={{ cursor: 'pointer', marginBottom: '10px' }}>
-              <img src={path} alt={`Sfondo ${id}`} style={{ width: '150px', height: '90px' }} />
+      <button type="button" style={styles.exit} onClick={onClose}><h4>X</h4></button>
+      <div className='row m-5'>
+        {Object.entries(backgrounds).map(([id, path]) => (
+          <div className='col' key={id} onClick={() => selectBackground(id)} style={{ cursor: 'pointer', marginBottom: '10px' }}>
+            <img src={path} alt={`Sfondo ${id}`} style={{ width: '150px', height: '90px' }} />
+          </div>
+        ))}
+      </div>
+      <div className='d-flex justify-content-center' style={{padding: "2vh"}}>
+        <form className='' onSubmit={handleSubmit}>
+          <div>
+            {/* <label className="form-label"><strong>Title</strong></label> */}
+            <input type="text" name="title" style={styles.input} value={board.title} onChange={handleInputChange} placeholder="Title" />
+          </div>
+
+          <div>
+            {/* <label className="form-label"><strong>Description</strong></label> */}
+            <input type="text" name="description" style={styles.input} value={board.description} onChange={handleInputChange} placeholder="Descrizione" />
+          </div>
+
+          <div>
+            <input type="checkbox" name="visible" checked={board.visible} onChange={handleInputChange} placeholder="Visibilita" />
+            <label className="form-label ps-2">Visible</label>
+          </div>
+
+          <div className='d-flex justify-content-center'>
+            <div>
+              <button type="submit" style={styles.button}><h5>Crea</h5></button>
             </div>
-          ))}
-        </div>
-        <div>
-          {/* <label className="form-label"><strong>Title</strong></label> */}
-          <input type="text" name="title" value={board.title} onChange={handleInputChange} placeholder="Title" />
-        </div>
-
-        <div>
-          {/* <label className="form-label"><strong>Description</strong></label> */}
-          <input type="text" name="description" value={board.description} onChange={handleInputChange} placeholder="Descrizione" />
-        </div>
-
-        <div>
-          {/* <label className="form-label"><strong>Visible</strong></label> */}
-          <input type="checkbox" name="visible" checked={board.visible} onChange={handleInputChange} placeholder="Visibilita" />
-        </div>
-
-        <button type="submit">Crea</button>
-        <button type="button" onClick={onClose}>Chiudi</button>
-      </form>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
+const styles = {
+  input: {
+    display: 'block',
+    width: '70vw',
+    padding: '10px',
+    margin: '10px 0',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+  },
+  button: {
+    background: '#aec5e5',
+    color: 'white',
+    border: 'none',
+    padding: '10px 40px',
+    margin: '10px 2px',
+    width: '70vw',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+  },
+  exit: {
+    border: 'none',
+    background: 'none',
+    color : '#8ecdf6'
+  }
+};
+
 export default NewBoardForm;
+
