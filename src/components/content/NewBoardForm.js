30dafccd036d6365dad23de654ed6c0c5ebda0b6
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-03-26 11:46:40 +0100

    sfondiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- src/components/content/NewBoardForm.js
+++ src/components/content/NewBoardForm.js
@@ -4,15 +4,17 @@ import axios from 'axios';
 import BackgroundSelector from '../common/BackgroundSelector';
 import { currentUser } from '../../App';
 import { useAtom } from 'jotai';
+import backgrounds from '../../assets/background';
 
 const NewBoardForm = ({ onClose, onBoardAdded }) => {
   const [user, setUser] = useAtom(currentUser);
   const [board, setBoard] = useState(
     {
-      userId:user.id,
+      userId: user.id,
       title: "",
       description: "",
-      visible: ""
+      visible: "",
+      img: ""
     }
   );
 
@@ -21,11 +23,11 @@ const NewBoardForm = ({ onClose, onBoardAdded }) => {
     // Usa il valore 'checked' per i checkbox, altrimenti usa 'value'
     const inputValue = type === 'checkbox' ? checked : value;
     setBoard({ ...board, [name]: inputValue });
-    console.log(board);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    console.log(board);
     try {
       await axios.post('/boards', board);
       onBoardAdded(); // Chiama questa funzione per aggiornare l'elenco delle bacheche
@@ -33,6 +35,10 @@ const NewBoardForm = ({ onClose, onBoardAdded }) => {
     } catch (error) {
       console.error('Error adding new board:', error);
     }
+  };
+
+  const selectBackground = async (id) => {
+    setBoard({ ...board, img: Number(id)});
   };
 
   return (
@@ -50,7 +56,13 @@ const NewBoardForm = ({ onClose, onBoardAdded }) => {
       borderRadius: '5px', // Bordi arrotondati (opzionale)
     }}>
       <form onSubmit={handleSubmit}>
-        <BackgroundSelector />
+        <div className='row m-5'>
+          {Object.entries(backgrounds).map(([id, path]) => (
+            <div className='col' key={id} onClick={() => selectBackground(id)} style={{ cursor: 'pointer', marginBottom: '10px' }}>
+              <img src={path} alt={`Sfondo ${id}`} style={{ width: '150px', height: '90px' }} />
+            </div>
+          ))}
+        </div>
         <div>
           {/* <label className="form-label"><strong>Title</strong></label> */}
           <input type="text" name="title" value={board.title} onChange={handleInputChange} placeholder="Title" />
@@ -63,7 +75,7 @@ const NewBoardForm = ({ onClose, onBoardAdded }) => {
 
         <div>
           {/* <label className="form-label"><strong>Visible</strong></label> */}
-          <input type="checkbox"  name="visible" checked={board.visible} onChange={handleInputChange} placeholder="Visibilita" />
+          <input type="checkbox" name="visible" checked={board.visible} onChange={handleInputChange} placeholder="Visibilita" />
         </div>
 
         <button type="submit">Crea</button>
--- src/components/content/YourBoards.js
+++ src/components/content/YourBoards.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import backgrounds from '../../assets/background';
 
 export default function YourBoards(props) {
 
@@ -7,13 +7,18 @@ export default function YourBoards(props) {
         <div className="row" >
             <h2>Bacheche</h2>
             {props.boards.map((board, index) => (
-                <div className='col-4 px-3' key={board.id || index}>
-                    <div className="card">
+                <div className='col-4 px-3' key={board.id || index} >
+                    <div className="card m-3"
+                        onClick={() => props.setBoard(board)}
+                        style={{
+                            backgroundImage: `url(${backgrounds[board.img]})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                            minHeight: '20vh'
+                        }}>
                         <div className="card-body">
-                            <h5 className="card-title">{board.title}</h5>
-                            <button type="button" className="btn btn-primary" onClick={() => props.setBoard(board)}>
-                                Scegli
-                            </button>
+                            <h5 className="card-title text-light">{board.title}</h5>
                         </div>
                     </div>
                 </div>
