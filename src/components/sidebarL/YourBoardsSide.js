f946aea431a093e7c3c45a5f88535ffd24079447
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-03-26 12:01:58 +0100

    sfondiiiiiiiiiiiiiiiii2

--- src/components/sidebarL/YourBoardsSide.js
+++ src/components/sidebarL/YourBoardsSide.js
@@ -1,6 +1,6 @@
 // YourBoardsSide.js
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import backgrounds from '../../assets/background';
 import NewBoardForm from '../content/NewBoardForm';
 
 
@@ -13,11 +13,26 @@ export default function YourBoardsSide(props) {
                 <div className="col-10 btn btn-danger" onClick={() => window.location.reload()}><h5>Le tue bacheche</h5></div>
                 <div className="col-2 btn btn-warning" onClick={() => setShowNewBoardForm(true)}><h1>+</h1></div>
             </div>
-            {props.boards.map((board, index) => (
-                <div key={board.id || index}>
-                    {board.title}
-                </div>
-            ))}
+            <div className='mt-2'>
+                {props.boards.map((board, index) => (
+                    <div className='d-flex ' key={board.id || index} >
+                        <div className="my-1 me-2 rounded-1"
+                            onClick={() => props.setBoard(board)}
+                            style={{
+                                backgroundImage: `url(${backgrounds[board.img]})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                                height: '4vh',
+                                width: '3vw'
+                            }}>
+                        </div>
+                        <div className='text-light'>
+                            {board.title}
+                        </div>
+                    </div>
+                ))}
+            </div>
             {showNewBoardForm && <NewBoardForm onClose={() => setShowNewBoardForm(false)} onBoardAdded={props.handleNewBoardAdded} />}
         </div>
     );
